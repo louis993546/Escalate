07336e8d0789c47fe376b8907afa23b535fb3c36
Louis <louis993546@gmail.com>
2023-12-06 01:11:40 +0100

    More popovers

--- Escalate/History/Details/SetsRowView.swift
+++ Escalate/History/Details/SetsRowView.swift
@@ -11,8 +11,10 @@ struct SetsRowView: View {
     @AppStorage("dragSensitivity") private var dragSensitivity = 24
     
     // TODO: (double) tap => Edit mode
-    @State private var isPopoverOpen = false
-    @State private var repsDiff: Float = 0
+    @State private var isSetsPopoverOpen = false
+    @State private var isWeightPopoverOpen = false
+    @State private var isRepsPopoverOpen = false
+    @State private var dragDiff: Float = 0
     
     let set: Sets
     let onSetChange: ((Int) -> Void)
@@ -29,22 +31,59 @@ struct SetsRowView: View {
             Text(String(set.reps.count))
                 .longPressAndDrag(
                     dragSensitivity: dragSensitivity,
-                    isDragging: $isPopoverOpen,
-                    offset: $repsDiff
+                    isDragging: $isSetsPopoverOpen,
+                    offset: $dragDiff
                 )
-                .popover(isPresented: $isPopoverOpen) {
-                    Text(String(max(set.reps.count + Int(repsDiff.rounded()), 1)))
+                .popover(isPresented: $isSetsPopoverOpen) {
+                    Text(String(max(set.reps.count + Int(dragDiff.rounded()), 1)))
                         .presentationCompactAdaptation((.popover))
                 }
-                .onChange(of: isPopoverOpen, initial: false) { oldValue, newValue  in
+                .onChange(of: isSetsPopoverOpen, initial: false) { oldValue, newValue  in
                     if oldValue == true && newValue == false { // i.e. just got closed
-                        onSetChange(Int(repsDiff.rounded()))
+                        onSetChange(Int(dragDiff.rounded()))
                     } else if oldValue == false && newValue == true { // i.e. just got opened
                         UINotificationFeedbackGenerator().notificationOccurred(.success)
                     }
                 }
             Text(String(set.getCommonWeight()?.clean ?? "WTF"))
-            Text(String(set.getCommonReps() ?? 0)) + Text(" \(set.remark ?? "")").font(.footnote)
+                .longPressAndDrag(
+                    dragSensitivity: dragSensitivity,
+                    isDragging: $isWeightPopoverOpen,
+                    offset: $dragDiff
+                )
+                .popover(isPresented: $isWeightPopoverOpen) {
+                    Text("\(max(0, dragDiff + (set.getCommonWeight() ?? 0)).round(nearest: 0.5).decimalPlaces(decimalPlaces: 1))")
+                        .presentationCompactAdaptation((.popover))
+                }
+                .onChange(of: isWeightPopoverOpen, initial: false) { oldValue, newValue  in
+                    if oldValue == true && newValue == false { // i.e. just got closed
+                        // TODO: save data
+                        print("Final weigh diff: \(dragDiff)")
+                    } else if oldValue == false && newValue == true { // i.e. just got opened
+                        UINotificationFeedbackGenerator().notificationOccurred(.success)
+                    }
+                }
+            Text(String(set.getCommonReps() ?? 0))
+                .longPressAndDrag(
+                    dragSensitivity: dragSensitivity,
+                    isDragging: $isRepsPopoverOpen,
+                    offset: $dragDiff
+                )
+                .popover(isPresented: $isRepsPopoverOpen) {
+                    //                    Text(String(max(set.getCommonReps() ?? 0 + Int(dragDiff.rounded()), 0)))
+                    Text("\(max(0, (Int(dragDiff.rounded()) + (set.getCommonReps() ?? 0))))")
+                    .presentationCompactAdaptation((.popover))
+                }
+                .onChange(of: isRepsPopoverOpen, initial: ((set.getCommonReps() ?? 0) != 0)) { oldValue, newValue in
+                    if oldValue == true && newValue == false { // i.e. just got closed
+                        // TODO: save data
+                        print("Final reps diff: \(dragDiff)")
+                    } else if oldValue == false && newValue == true { // i.e. just got opened
+                        UINotificationFeedbackGenerator().notificationOccurred(.success)
+                    }
+                }
+            // TODO: put remark back
+            //            + Text(" \(set.remark ?? "")").font(.footnote)
         }.if({return set.skipped}()) { view in
             view.foregroundStyle(.gray)
         }
@@ -52,8 +91,6 @@ struct SetsRowView: View {
 }
 
 struct LongPressAndDrag: ViewModifier {
-    @State private var repsDiff: Float = 0
-    
     let dragSensitivity: Int
     @Binding var isDragging: Bool
     @Binding var offset: Float
@@ -65,7 +102,10 @@ struct LongPressAndDrag: ViewModifier {
                 isDragging = true
             }
         let dragGesture = DragGesture()
-            .onChanged { value in offset = Float(value.translation.width) / Float(dragSensitivity) }
+            .onChanged { value in
+                print("\(Float(value.translation.width) / Float(dragSensitivity))")
+                offset = Float(value.translation.width) / Float(dragSensitivity)
+            }
             .onEnded { _ in
                 isDragging = false
             }
@@ -95,6 +135,21 @@ extension View {
 extension Float {
     var clean: String {
         return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+    
+    func decimalPlaces(decimalPlaces: Int) -> String {
+        String(format: "%.\(decimalPlaces)f", self)
+    }
+    
+    func round(nearest: Float) -> Float {
+        let n = 1 / nearest
+        let numberToRound = self * n
+        return numberToRound.rounded() / n
+    }
+    
+    func floor(nearest: Float) -> Float {
+        let intDiv = Float(Int(self / nearest))
+        return intDiv * nearest
     }
 }
 
